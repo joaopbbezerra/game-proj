abb9cf4ad2eb5fb295f72d4a7f9065835f1d3171
Joao Pedro <joaopbbezerra@gmail.com>
2021-06-13 15:14:07 +0100

    score, start again, newImage

--- js/Retan.js
+++ js/Retan.js
@@ -1,47 +1,16 @@
-class Retan{
-    constructor(){
-        this.x = 490
-        this.y = 520
-        this.width= 120
-        this.height = 180
+class Window {
+    constructor(x, y){
+        this.x = x // Na hora de chamar o x e o y vamos usar o do fireman - o width no caso do x, o y deve permanecer o mesmo
+        this.y = y
+        this.width = 60 //Isso vai ser automático, vamos usar sempre 
+        this.height = 60 // o mesmo pq a janela vai ter tamanho fixo
     }
-    draw(){
-        context.fillStyle = "rgba(0, 0, 200, 0)"; //"rgba(0, 0, 200, 0)" transparente
-        context.fillRect(this.x, this.y, this.width, this.height)
-        // setInterval(() => { //Opção de tirar o draw de dentro do set interval e o fogo descer suave
-        //     this.draw()    
-        // }, 90); Se colocar o draw no start game ele já funciona, mas a velocidade tá ficando infinita.
-    }
-    move(key){
-        context.clearRect(this.x, this.y, this.width, this.height)
-        switch(key){
-            case "ArrowUp":
-                if (this.y > 230){
-                    this.y -= 230; //Pra andar pra cima vai diminuir 25 pixels
-                }
-            break;
-            case "ArrowDown":
-                if (this.y < 460){
-                    this.y += 230
-                }
-            break;
-            case "ArrowLeft":
-                if (this.x > 200){
-                    this.x -= 200
-                }
-            break;
-            case "ArrowRight":
-                if (this.x < 400){
-                    this.x += 200
-                }
-            break;
-            // case "Spacebar":
-            //     this.extinguishFire() Implementar caso tudo corra corretamente
-        }
-    }
-    extinguishFire(x, y){ //Vai passar os argumentos pra saber onde a janela vai ser colocada
+    drawWindow(){
         const image = new Image ()
-        image.src = "" //Imagem da janela sem fogo
-        context.drawImage(image, x, y, 50, 50)
+        image.src = "./image/fire_Nobackground.png" //Imagem do foguinho
+        // setInterval(() => {
+        //     this.drawFire()
+        // }, 90);
+        context.drawImage(image, this.x, this.y, this.width, this.height)
     }
 }
--- js/index.js
+++ js/index.js
@@ -31,7 +31,7 @@ function updateCanvas(){
     context.clearRect(0, 0, buildingCanvas.clientWidth, buildingCanvas.clientHeight)
     newGame.fireMan.draw()
     newGame.firesFreq++
-    if (newGame.firesFreq % 20 === 1){
+    if (newGame.firesFreq % 60 === 1){
         const randomFiresX = Math.floor(Math.random() *buildingCanvas.clientWidth)
         const randomFireY = -100
         //Random do tamanho caso consigamos implementar tudo - fase 2 e 3
@@ -49,13 +49,20 @@ function updateCanvas(){
         if (collision(fire)){
             newGame.gameOver = true
             newGame.firesFreq = 0
+            alert(`Game Over! Final Score: ${newGame.score}`)
             newGame.score = 0
             newGame.fires = []
-            document.getElementById("canvas").style.display = "none"
+            document.getElementById("score").innerHTML = 0
+            // document.getElementById("canvas").style.display = "none" //Comentado pq tava bugando o score. 
+            //O score tava deletando o fogo que ainda tinha na imagem e somando no score.
+            //Duas opções: 1 - Mudar a forma de contagem de fogo para tempo
+            // 2 - Mudar a forma como ele verifica ou simplesmente deixar ele sem colocar o "none" que é o que faz apagar o width e contar os pontos a mais
             cancelAnimationFrame(newGame.animationId)
-            alert("Game Over")
+            
         }
         if (fire.y > buildingCanvas.clientHeight){
+            newGame.score++
+            document.getElementById("score").innerHTML = newGame.score
             newGame.fires.splice(index, 1)
         }
     })
